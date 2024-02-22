12df77f97ec68ddbff41ad6e2a69cf2b11639f0c
Justin <isaac_justin@hotmail.com>
2024-02-22 17:41:59 -0600

    formato corregido

--- tipos/07-numeros-funciones.py
+++ tipos/07-numeros-funciones.py
@@ -1,13 +1,28 @@
 import math
 
+# Redondea al numero mas cercanos, en este caso a 1
 print(round(1.3))
+
+# Redondea al numero mas cercanos, en este caso a 2
 print(round(1.8))
+
+# Redondea al numero mas cercanos, en este caso a 2 por estar justo en la mitad
 print(round(1.5))
+
+# Nos da el valor absoluto de un numero
 print(abs(-5))
 
-
+# Redondea al numero mas superior cercano, en este caso a 2
 print(math.ceil(1.3))
+
+# Redondea al numero mas inferior cercano, en este caso a 1
 print(math.floor(1.9))
-print(math.isnan(1852))
+
+# Redondea al numero mas superior cercanos, en este caso a 2
+# print(math.isnan("hh"))
+
+# Eleva el primero numero a la potencia indicada en el segundo numero
 print(math.pow(5, 3))
+
+# Nos da la raiz cuadrada del numero dado
 print(math.sqrt(9))
