f20ad0e4a2357f82644beb222d6025012850a26c
Justin <isaac_justin@hotmail.com>
2024-02-23 11:38:54 -0600

    terminamos modulo de numeros

---
+++ tipos/08-calculadora.py
@@ -0,0 +1,18 @@
+n1 = input('Ingresa un numero ')
+n2 = input('Ingresa un segundo numero ')
+
+n1 = int(n1)
+n2 = int(n2)
+
+suma = n1 + n2
+resta = n1 - n2
+multipicacion = n1 * n2
+division = n1 / n1
+
+mensaje = f"""
+Para los numeros {n1} y {n2},
+la suma es {suma},
+la resta es {resta}
+la multiplicacion es {multipicacion},
+y la division es {division}
+"""
---
+++ tipos/09-conversion-tipos.py
@@ -0,0 +1,14 @@
+# input resive todo como texto
+x = input()
+
+# Convierte un numero en formato texto a un numero en formato entero
+int()
+
+# Convierte todo lo que le pases a un texto
+str()
+
+# Convierte lo que le pases a un flotante
+
+# Bool evalua valores trusty y falsy y devuelve true o False segun el caso
+# valores Falsy son "", el objeto None y el 0
+bool()
